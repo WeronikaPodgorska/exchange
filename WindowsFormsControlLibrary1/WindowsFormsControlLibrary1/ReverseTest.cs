1cf9a1f31c6e6fad4278249d04875347e64fcbd5
WeronikaPodgorska <31224467+WeronikaPodgorska@users.noreply.github.com>
2021-05-26 00:27:29 +0200

    Add files via upload

---
+++ WindowsFormsControlLibrary1/WindowsFormsControlLibrary1/ReverseTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace ReverseTest
+{
+    [TestFixture]
+    public class NowaKlasa
+    {
+        [Test]
+        public void ReveresedTextMatchesExpected()
+        {
+            const string text = "Ala ma kota";
+            const string expected = "kota ma Ala";
+
+            Reverse(text).Should().Be(expected, "Reversed text should match expected");
+        }
+
+        public static string Reverse(string text)
+        {
+            return string.Join(" ", text?.Split(' ').Reverse().ToArray());
+        }
+    }
+}
